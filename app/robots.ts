88580f2e627579928e08ffc4d4999ee17b72021f
Robert Nichols <dknyrob@gmail.com>
2026-04-02 23:47:18 +0800

    fix: replace generateSitemaps with static XML files, named sitemap-core/ages/ideal-weight/programmatic

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,13 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: "*", allow: "/", disallow: ["/api/", "/_next/"] }],
-    sitemap: "https://www.bmicalculatorsingapore.com/sitemap.xml",
+    sitemap: [
+      "https://www.bmicalculatorsingapore.com/sitemap.xml",
+      "https://www.bmicalculatorsingapore.com/sitemap-core.xml",
+      "https://www.bmicalculatorsingapore.com/sitemap-ages.xml",
+      "https://www.bmicalculatorsingapore.com/sitemap-ideal-weight.xml",
+      "https://www.bmicalculatorsingapore.com/sitemap-programmatic.xml",
+    ],
     host: "https://www.bmicalculatorsingapore.com",
   };
 }
