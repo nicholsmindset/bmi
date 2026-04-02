559a2c0a7ff3a606edf60eb649bacee79f8dae74
Robert Nichols <dknyrob@gmail.com>
2026-04-03 00:14:59 +0800

    feat: update domain to bmicalculatorsingapore.com, add GSC verification, add segmented sitemaps

--- app/robots.ts
+++ app/robots.ts
@@ -6,9 +6,10 @@ export default function robots(): MetadataRoute.Robots {
     sitemap: [
       "https://www.bmicalculatorsingapore.com/sitemap.xml",
       "https://www.bmicalculatorsingapore.com/sitemap-core.xml",
-      "https://www.bmicalculatorsingapore.com/sitemap-ages.xml",
+      "https://www.bmicalculatorsingapore.com/sitemap-bmi-age.xml",
       "https://www.bmicalculatorsingapore.com/sitemap-ideal-weight.xml",
-      "https://www.bmicalculatorsingapore.com/sitemap-programmatic.xml",
+      "https://www.bmicalculatorsingapore.com/sitemap-protein.xml",
+      "https://www.bmicalculatorsingapore.com/sitemap-calories-burned.xml",
     ],
     host: "https://www.bmicalculatorsingapore.com",
   };
